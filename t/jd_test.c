31ffa79a8835a5b05d72740cf57c0c254659e6dc
Andy Armstrong <andy@hexten.net>
2013-01-22 20:56:24 +0000

    Add jdt_is_string.

--- t/jd_test.c
+++ t/jd_test.c
@@ -12,15 +12,21 @@ void jdt_dump(const char *label, jd_var *v) {
 }
 
 static int _is(jd_var *got, jd_var *want, const char *msg, va_list ap) {
+  int rc;
+  rc = test(jd_compare(got, want) == 0, msg, ap);
+  if (!rc) {
+    jdt_dump("wanted", want);
+    jdt_dump("got", got);
+  }
+  return rc;
+}
+
+static int _isj(jd_var *got, jd_var *want, const char *msg, va_list ap) {
   jd_var vgot = JD_INIT;
   int rc;
 
   jd_to_json(&vgot, got);
-  rc = test(jd_compare(&vgot, want) == 0, msg, ap);
-  if (!rc) {
-    jdt_dump("wanted", want);
-    jdt_dump("got", &vgot);
-  }
+  rc = _is(&vgot, want, msg, ap);
   jd_release(&vgot);
   return rc;
 }
@@ -32,13 +38,26 @@ int jdt_is(jd_var *got, jd_var *want, const char *msg, ...) {
 
   jd_to_json(&vwant, want);
   va_start(ap, msg);
-  rc = _is(got, &vwant, msg, ap);
+  rc = _isj(got, &vwant, msg, ap);
   va_end(ap);
   jd_release(&vwant);
   return rc;
 }
 
 int jdt_is_json(jd_var *got, const char *want, const char *msg, ...) {
+  va_list ap;
+  jd_var vwant = JD_INIT;
+  int rc;
+
+  jd_set_string(&vwant, want);
+  va_start(ap, msg);
+  rc = _isj(got, &vwant, msg, ap);
+  va_end(ap);
+  jd_release(&vwant);
+  return rc;
+}
+
+int jdt_is_string(jd_var *got, const char *want, const char *msg, ...) {
   va_list ap;
   jd_var vwant = JD_INIT;
   int rc;
