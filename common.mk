3fc83d6aa2112bed6f51d5c69f0317609fcd2feb
Andy Armstrong <andy@hexten.net>
2013-02-21 12:57:47 +0000

    Coverage testing.

--- common.mk
+++ common.mk
@@ -1,6 +1,10 @@
 CFLAGS=-Wall -Werror -g2 -D_LARGEFILE64_SOURCE
+LDFLAGS=-lc -lm
 ifneq ($(shell $(CC) -v 2>&1 | grep -i clang),)
 CFLAGS+=-Qunused-arguments
 endif
-LDFLAGS=-lc -lm
+ifneq ($(PROFILE),)
+CFLAGS+=-fprofile-arcs -ftest-coverage
+LDFLAGS+=-lgcov
+endif
 
---
+++ tools/cover.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -x
+find . \( -name '*.gcno' -o -name '*.gcda' -o -name '*.gcov' \
+  -o -name '*.info' \) -print0 | xargs -0 rm
+rm -rf cover
+make clean && make PROFILE=yes
+
+infos=""
+for test in t/*.t; do
+  bin=${test%.t}
+  target=$( basename $bin )
+  info="$target.info"
+  result="cover/$target"
+  make -C t $target PROFILE=yes
+  find . -name '*.gcda' -print0 | xargs -0 rm
+  ./$bin
+  lcov -t "$bin" -o "$info" -c -d .
+  infos="$infos -a $info"
+  genhtml -o "$result" "$info"
+done
+lcov -o "all.info" $infos
+genhtml -o "cover/all" "all.info"
+find . \( -name '*.gcno' -o -name '*.gcda' -o -name '*.gcov' \
+  -o -name '*.info' \) -print0 | xargs -0 rm
+
+# vim:ts=2:sw=2:sts=2:et:ft=sh
+
