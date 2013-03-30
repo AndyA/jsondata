4bdbdde477b80779f1bd881b662179c610f7bf9b
Andy Armstrong <andy@hexten.net>
2013-03-30 19:02:39 +0000

    Warn if any symbols are duplicated.
    Because that would have a very confusing effect.

--- tools/fix_private.pl
+++ tools/fix_private.pl
@@ -12,10 +12,19 @@ use constant PRIVATE => 'jd_private.h';
 my @bad_public  = my @good_public  = read_header(PUBLIC);
 my @bad_private = my @good_private = read_header(PRIVATE);
 
-s/^jd_+/jd__/ for @bad_public;
-s/^jd_+/jd_/  for @good_public;
-s/^jd_+/jd_/  for @bad_private;
-s/^jd_+/jd__/ for @good_private;
+s/^jd_+/jd__/ for @bad_public,  @good_private;
+s/^jd_+/jd_/  for @good_public, @bad_private;
+
+my %seen = map { $_ => 1 } @good_public;
+$seen{$_}++ for @bad_private;
+my @dup = grep { $seen{$_} > 1 } keys %seen;
+if (@dup) {
+  print "The following symbols appear in both ", PUBLIC, " and ", PRIVATE,
+   ":\n";
+  print "  $_\n" for sort @dup;
+  print "\nPlease fix and try again.\n";
+  exit 1;
+}
 
 my %fixup = ();
 @fixup{ @bad_public, @bad_private } = ( @good_public, @good_private );
